30161811b4170527150e3207c173d578bbe59d4b
alejandro-castillo-delgado <alejandro.castillo.delgado.97@gmail.com>
2024-10-12 00:34:07 -0500

    added graph of the Lates Invoices on the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import RenueveChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import { lusitana } from '../ui/fonts';
-import { revenue } from '../lib/placeholder-data';
-import { fetchRevenue } from '../lib/data';
+//import { revenue } from '../lib/placeholder-data';
+import { fetchRevenue, fetchLatestInvoices } from '../lib/data';
+
 
 
 const Dashboard = async () => {
-    const fetchedRevenue = await fetchRevenue();
+    const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
+
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -17,6 +20,7 @@ const Dashboard = async () => {
             </div>
             <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
                 <RenueveChart revenue={revenue}/>
+                <LatestInvoices latestInvoices={latestInvoices}/>
             </div>
         </main>
     )
