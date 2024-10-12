40bc50a2b500c38c13f6b96eac74bc264a83262c
alejandro-castillo-delgado <alejandro.castillo.delgado.97@gmail.com>
2024-10-12 00:02:57 -0500

    I added the bar graph to the administration panel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import RenueveChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { lusitana } from '../ui/fonts';
+import { revenue } from '../lib/placeholder-data';
+import { fetchRevenue } from '../lib/data';
 
-const Dashboard = () => {
+
+const Dashboard = async () => {
+    const fetchedRevenue = await fetchRevenue();
     return (
-        <div>Dashboard</div>
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+            </div>
+            <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+                <RenueveChart revenue={revenue}/>
+            </div>
+        </main>
     )
 }
 
