0dd3bcae4a92305b8cef9e58897f6853b9bc91ed
alejandro-castillo-delgado <alejandro.castillo.delgado.97@gmail.com>
2025-01-09 00:25:22 -0500

    added Breadcrumbs in Customer Form Create

--- app/dashboard/customers/create/page.tsx
+++ app/dashboard/customers/create/page.tsx
@@ -1,13 +1,23 @@
 import CustomerForm from '@/app/ui/customers/create-form.'
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import React from 'react'
 
 const CreateCustomerPage = async () => {
 
     return (
-        <div>
-            Customer FORM
+        <main>
+            <Breadcrumbs
+                    breadcrumbs={[
+                        { label: 'Customers', href: '/dashboard/customers' },
+                        {
+                            label: 'Create Customer',
+                            href: '/dashboard/customers/create',
+                            active: true
+                        },
+                    ]}
+            />
             <CustomerForm />
-        </div>
+        </main>
     )
 }
 
