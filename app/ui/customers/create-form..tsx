8b6b5630f7f4ee35c1a9c7e53ca9290eaada0748
alejandro-castillo-delgado <alejandro.castillo.delgado.97@gmail.com>
2025-01-09 00:16:01 -0500

    visual enhancement of the DropZone Image

--- app/ui/customers/create-form..tsx
+++ app/ui/customers/create-form..tsx
@@ -7,6 +7,7 @@ import { createCustomer, StateCustomer } from '@/app/lib/actions';
 import { useActionState, useState } from 'react';
 import ImageUpload from './upload';
 import { useDropzone } from 'react-dropzone';
+import Image from 'next/image';
 
 export default function CustomerForm() {
 
@@ -84,15 +85,33 @@ export default function CustomerForm() {
                         ))}
                     </div>
                 </div>
-                <div className='h-10 w-full block text-center'>
-                    {/* <input type='file' name='image' className='' accept='image/*' id='image' /> */}
-                    <div {...getRootProps()}>
-                        <input name='image' {...getInputProps()} />
-                        <p>Arrastra y suelta una imagen aquí, o haz clic para seleccionar.</p>
-                        {selectedImage && <img src={URL.createObjectURL(selectedImage)} alt="Preview" />}
+                <div className="col-span-full">
+                    <label className="block text-sm/6 font-medium text-gray-900">Cover photo</label>
+                    <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                        <div className="text-center">
+                            <svg className="mx-auto size-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clip-rule="evenodd" />
+                            </svg>
+                            <div  {...getRootProps({ className: "mt-4 flex text-sm/6 text-gray-600" })} >
+                                <label className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                    <span>Upload a file</span>
+                                    <input  name='image' {...getInputProps()} />
+                                </label>
+                                <p className="pl-1">or drag and drop</p>
+                            </div>
+                            <p className="text-xs/5 text-gray-600">PNG, JPG, GIF up to 2MB</p>
+                            {selectedImage && <Image height={300} width={300} src={URL.createObjectURL(selectedImage)} alt="Preview" />}
+                        </div>
                     </div>
                 </div>
-                <button className='bg-blue-500 rounded-lg py-2 px-4 text-white font-bold' type='submit'>Subir</button>
+                {/* <div className="h-10 w-full block text-center bg-gray-100 border-2 border-dashed border-gray-400 rounded-lg p-4">
+                    <div {...getRootProps()} className="flex justify-center items-center">
+                        <input name='image' {...getInputProps()} />
+                        <p className="text-gray-600 text-sm">Arrastra y suelta una imagen aquí, o haz clic para seleccionar.</p>
+                        {selectedImage && <Image height={300} width={300} src={URL.createObjectURL(selectedImage)} alt="Preview" className="mt-4" />}
+                    </div>
+                </div> */}
+                <button className='bg-blue-500 rounded-lg py-2 px-4 mt-5 text-white font-bold' type='submit'>Subir</button>
             </div>
         </form>
     );
